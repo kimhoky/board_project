d34200da88264546c013472cbaf020256bbfa9d9
rydnjs98 <98crace@gmail.com>
2024-01-10 21:39:50 +0900

    DB 최신화 후 수정

--- board/src/main/java/com/stream/board/dto/MemberDTO.java
+++ board/src/main/java/com/stream/board/dto/MemberDTO.java
@@ -12,19 +12,17 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class MemberDTO { //회원 정보를 필드로 정의
-    private Long m_id;
-    private String m_email;
-    private String m_password;
-    private String m_name;
+    private String User_id;
+    private String User_password;
+    private String User_name;
 
     //lombok 어노테이션으로 getter,setter,생성자,toString 메서드 생략 가능
 
     public static MemberDTO toMemberDTO(MemberEntity memberEntity){
         MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setM_id(memberEntity.getM_id());
-        memberDTO.setM_email(memberEntity.getM_email());
-        memberDTO.setM_name(memberEntity.getM_name());
-        memberDTO.setM_password(memberEntity.getM_password());
+        memberDTO.setUser_id(memberEntity.getUser_id());
+        memberDTO.setUser_name(memberEntity.getUser_name());
+        memberDTO.setUser_password(memberEntity.getUser_password());
 
         return memberDTO;
     }
--- board/src/main/java/com/stream/board/entity/MemberEntity.java
+++ board/src/main/java/com/stream/board/entity/MemberEntity.java
@@ -8,29 +8,33 @@ import lombok.Setter;
 @Entity
 @Setter
 @Getter
-@Table(name = "member") //database에 해당 이름의 테이블 생성
+@Table(name = "user") //database에 해당 이름의 테이블 생성
 public class MemberEntity { //table 역할
     //jpa ==> database를 객체처럼 사용 가능
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
-    private Long m_id;
-
-    @Column(unique = true)
-    private String m_email;
+    private String User_id;
 
     @Column
-    private String m_password;
+    private String User_password;
 
     @Column
-    private String m_name;
+    private String User_name;
+
+    @Column
+    private String User_favorite;
+
+    @Column
+    private String User_profile;
+
+    @Column
+    private int Is_streamer;
 
     public static MemberEntity toMemberEntity(MemberDTO memberDTO){
         MemberEntity memberEntity = new MemberEntity();
-        memberEntity.setM_id(memberDTO.getM_id());
-        memberEntity.setM_email(memberDTO.getM_email());
-        memberEntity.setM_name(memberDTO.getM_name());
-        memberEntity.setM_password(memberDTO.getM_password());
+        memberEntity.setUser_id(memberDTO.getUser_id());
+        memberEntity.setUser_name(memberDTO.getUser_name());
+        memberEntity.setUser_password(memberDTO.getUser_password());
         return memberEntity;
     }
 
