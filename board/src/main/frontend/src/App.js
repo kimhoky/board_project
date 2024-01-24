37aa5e966883eac8acb5899cb017aecf7b8ec1ee
Kimhoky <enwl77@naver.com>
2024-01-24 19:17:00 +0900

    ㅇㅇ

--- board/src/main/frontend/src/App.js
+++ board/src/main/frontend/src/App.js
@@ -5,12 +5,14 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./SignUp"; // 경로가 정확한지 확인
 import Login from "./Login"; // 경로가 정확한지 확인
+import Main from "./pages/Main";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
+         <Route path="/Main" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Add2Component />} />
       </Routes>
--- board/src/main/frontend/src/add_2.js
+++ board/src/main/frontend/src/add_2.js
@@ -2,17 +2,12 @@ import { Link, Routes, Route } from "react-router-dom";
 
 import Header from "./common/Header";
 import Footer from "./common/Footer";
-import Main from "./pages/Main";
+
 const Add2Component = () => {
   return (
     <>
       <Header />
 
-      <Routes>
-        <Route path="/Main" element={<Main />} />
-
-        <Route path="*" element={<>이런 페이지는 없음</>} />
-      </Routes>
       <Footer />
     </>
   );
