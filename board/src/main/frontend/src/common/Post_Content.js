678dbc10094057f482623657f3f04f268995c5de
vahallla <89891803+vahallla@users.noreply.github.com>
2024-01-29 21:30:35 +0900

    post 페이지 임시완성

--- board/src/main/frontend/src/common/Post_Content.js
+++ board/src/main/frontend/src/common/Post_Content.js
@@ -1,11 +1,82 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "../css/content.css";
 
 export default function Post_Content() {
   return (
-    <form>
-      <input className="title" placeholder="제목을 입력해주세요"></input>
-    </form>
+    <div className="post_container">
+      <div className="post_title">
+        <form>
+          <span>말머리</span>
+
+          <input className="title" placeholder="제목을 입력해주세요"></input>
+        </form>
+        <button className="block">차단하기</button>
+        <span>작성자</span>
+      </div>
+      <div className="post_content">
+        <div className="post_top">
+          <form>
+            <div className="dt">
+              <span className="date">2024-01-08</span>
+              <span className="time">00:00:00</span>
+
+              <span className="test">댓글</span>
+              <span className="recommend">추천</span>
+              <span className="view">조회수</span>
+            </div>
+          </form>
+        </div>
+        <div className="post_text">
+          <div className="post_button">
+            <Link to="1">
+              <span>삭제</span>
+            </Link>
+            <Link to="1">
+              <span>수정</span>
+            </Link>
+            <Link to="1">
+              <span>공유</span>
+            </Link>
+            <Link to="1">
+              <span>신고</span>
+            </Link>
+          </div>
+          <form>
+            <textarea className="post_input">3232</textarea>
+          </form>
+          <div className="gd">
+            <button className="good">좋아요:0</button>
+            <button className="bad">싫어요:0</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="comment-container">
+        <hr></hr>
+        <div className="comment-header">
+          <div className="user-info">
+            <img
+              src="프로필 사진 경로"
+              alt="프로필 사진"
+              className="profile-img"
+            />
+            <span className="nickname">닉네임</span>
+          </div>
+          <div className="actions">
+            <span className="time">시간</span>
+            <button className="edit">수정</button>
+            <button className="delete">삭제</button>
+            <button className="report">신고</button>
+          </div>
+        </div>
+        <div className="comment-content">
+          <from>
+            <textarea className="comment"></textarea>
+            <button className="registration">등록</button>
+          </from>
+        </div>
+      </div>
+    </div>
   );
 }
